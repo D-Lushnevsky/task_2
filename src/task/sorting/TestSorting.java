de523b930bab8ca74a384be1a7948925b5100fd5
Denis Lushnevskiy <fantazer171@mail.ru>
2021-12-17 10:07:45 +0700

    1

--- src/task/sorting/TestSorting.java
+++ src/task/sorting/TestSorting.java
@@ -4,4 +4,11 @@ public class TestSorting {
     public static void main(String args[]) {
 
     }
+
+    private static double[] generateArray() {
+        double[] arrayGen = {15, 3, 65, 60, 6, 34, 22, 21};
+        return arrayGen;
+    }
 }
+
+
