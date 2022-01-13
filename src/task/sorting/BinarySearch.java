aefdf4c4d5525604a91a6f5c190318fc71c8cef2
Iuliia Liushnevskaya <fantazer171@mail.ru>
2022-01-13 08:40:22 +0700

    ignore

--- src/task/sorting/BinarySearch.java
+++ src/task/sorting/BinarySearch.java
@@ -6,16 +6,19 @@ public class BinarySearch {
         int high = array.length - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
+            /*
+            **пока искомое значение меньше значения в середине массива, вехрняя граница
+            * сдвигается вниз и наоборот.
+            * После уменьшения границ  массив снова делиться пополам и т.д.
+            * Так пока не станет value == mid
+             */
             if (value < array[mid]) {
                 high = mid - 1;
             } else if (value > array[mid]) {
                 low = mid + 1;
-
             } else {
                 return mid;
             }
-
         }
         return -1;
     }
